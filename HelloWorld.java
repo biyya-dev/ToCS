a1c163fbc4ab0ad16afa562377779a4d1500ec36
biyya-dev <areebaeman524@gmail.com>
2024-11-18 14:48:02 +0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int a=4;
+        int b=5;
+        int sum=a+b;
+        System.out.println("Sum is "+sum);
 
     }
 }
